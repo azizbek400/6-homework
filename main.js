f5800a368c4019a72939b06e2cedadad8bb8c978
azizbek400 <aziznormurodov400@gmail.com>
2024-03-08 23:44:24 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,63 @@
+// 22
+
+// function x(num) {
+
+//     for (let i = 2; i < num; i++) {
+//         if (num % i == 0) {
+//             return "tub son emas"
+//         }
+//     }
+
+//     return " tub son"
+// }
+
+// console.log(x(19));
+
+
+// 5
+
+// let n = 2;
+// let k = 8;
+
+// switch (k) {
+//     case 1:
+//     console.log( n  k);
+//     break;
+
+//     case 2:
+//     console.log( n  k);
+//     break;
+
+//     case 3:
+//     console.log( n  k);
+//     break;
+
+//     case 4:
+//     console.log( n  k);
+//     break;
+
+//     case 5:
+//     console.log( n  k);
+//     break;
+
+//     case 6:
+//     console.log( n  k);
+//     break;
+
+//     case 7:
+//     console.log( n  k);
+//     break;
+
+//     case 8:
+//     console.log( n  k);
+//     break;
+
+//     case 9:
+//     console.log( n  k);
+//     break;
+
+//     case 10:
+//     console.log( n  k);
+//     break;
+// }
+
